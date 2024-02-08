49815e9bedcecc0ec87186c48a3bccee5cf1b97d
Alexander <alexitorjlanus@gmail.com>
2024-02-08 07:48:21 -0300

    a borrar

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,12 +1,10 @@
-module_exports={
+{
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    "port":process.env.DB_PORT,
-
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
